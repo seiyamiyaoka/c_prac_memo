8cb99a2769ae9149799bdab4549aed3a4ab195fc
seiyamiyaoka <seiyamiyaoka@gmail.com>
2019-02-09 14:40:58 +0900

    第4章終了

--- main.c
+++ main.c
@@ -2,5 +2,9 @@
 int main(void) {
   printf("intel:\tPentinum4\n");
   printf("AMD\t:Atlon64\n");
+  printf("%d円\n%d円\n", 100, 500);
+  int a = 100;
+  int b = 200;
+  printf("%d円+%d円=%d円\n", a, b, a+b);
   return 0;
 }
