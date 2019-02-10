861d5ebb3f6c915911d4f3716face8fa84fd93af
seiyamiyaoka <seiyamiyaoka@gmail.com>
2019-02-10 16:18:25 +0900

    13章終了;

--- main2.c
+++ main2.c
@@ -1,13 +1,30 @@
 #include <stdio.h>
 
 int counter(void);
-int other_counter(void);
 
 int main(void) {
-  counter();
-  counter();
-  counter();
-  other_counter();
+  // int array[100];
+  // int narray[10] = {42, 79, 13};
+  // array[9] = 100;
+  // array[9]++;
+  // counter();
+  // counter();
+  // counter();
+  // printf("%d\n", array[9]);
+  // int i;
+  // int array[] = {1,2,3,5,6,7,8,10};
+  // int ary_size = sizeof(array) / sizeof(array[0]);
+  // for(i=0; i < ary_size; i++) {
+  //   printf("%dに割り当てられている数は%dです\n", i, array[i]);
+  // }
+
+  int ary[] = {1,2,3,4,5,6,7,9};
+  int size = sizeof(ary) / sizeof(ary[0]);
+  int i;
+
+  for (i=size; i >= 0; i--) {
+    printf("%d\n", ary[i]);
+  }
   return 0;
 }
 
@@ -15,10 +32,5 @@ int counter(void) {
   static int count;
   count ++;
   printf("カウント数は%dです\n", count);
-  return 0;
-}
-
-int other_counter(void) {
-  printf(count);
   return 0;
 }
